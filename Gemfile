013777698e766880b9233f12b8e254d493b17c50
Richard Reading <Richard.Reading@me.com>
2013-01-26 13:00:54 +0000

    added some code for testing, not yet working but getting there

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ end
 
 
 group :development, :test do
+  gem 'factory_girl_rails', '1.0'
   gem 'rspec-rails'
   gem 'debugger'
 end
--- spec/controllers/data_provider_users_controller_spec.rb
+++ spec/controllers/data_provider_users_controller_spec.rb
@@ -19,7 +19,14 @@ require 'spec_helper'
 # that an instance is receiving a specific message.
 
 describe DataProviderUsersController do
-
+  # include Devise::TestHelpers
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    user = Factory.create(:user)
+    # user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the confirmable module
+    sign_in user
+  end
+  
   # This should return the minimal set of attributes required to create a valid
   # DataProviderUser. As you add validations to DataProviderUser, be sure to
   # update the return value of this method accordingly.
