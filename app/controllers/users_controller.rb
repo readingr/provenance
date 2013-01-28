9aebba4294df49673d7c3c510b33926fc6b466c5
Richard Reading <Richard.Reading@me.com>
2013-01-28 17:09:36 +0000

    got it working using net http. Thus no storing of ajax requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -88,11 +88,44 @@ class UsersController < ApplicationController
   #this creates the provenance when the user logs in and sends it to the store
   def prov_login
 
+    require 'net/http'
+    require 'uri'
+
+    uri = URI.parse("http://127.0.0.1:8000/api/v0/bundle/2")
+
+    # debugger
+    http = Net::HTTP.new(uri.host, uri.port) 
+    # http.use_ssl = true
+    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(uri.path) 
+    request.add_field("Authorization", "ApiKey richard:53ba3c6f3f106162f0765424275be7d9461afd4a")
+
+    response = http.request(request)
+    case response 
+
+    when Net::HTTPRedirection then
+      uri = URI.parse(response['location'])
+      http = Net::HTTP.new(uri.host, uri.port) 
+      request = Net::HTTP::Get.new(uri.path) 
+      request.add_field("Authorization", "ApiKey richard:53ba3c6f3f106162f0765424275be7d9461afd4a")
+      response = http.request(request)
+    end
+    # debugger
+
+    puts "***************"
+    puts response.body
+    puts "***************"
+
+
+
+
 
     respond_to do |format|
       format.html { redirect_to root_path }
       format.json { head :no_content }
     end
   end
+
 
 end
